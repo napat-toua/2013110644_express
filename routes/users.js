bd7e30d21fc372398d3ed5e7519a46ce30429e08
Napat <napatotaku@gmail.com>
2022-11-26 11:26:17 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello world');
+  res.status(200).json({
+    fullname: 'Napat Touangam'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  //res.send('Hello world');
+  res.status(200).json({
+    fullname: 'Napat Touangam',
+    nickname: 'Yuu', 
+    hobby: 'Sleep',
+    gitusername: 'napat-toua'
+  })
 });
 
 module.exports = router;
