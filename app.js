5946bc809ca72b4e0291f9ffd9b80e2dbe05bae4
Napat <napatotaku@gmail.com>
2022-12-10 11:45:33 +0700

    Connect DB

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var companyRouter = require('./routes/company')
 
 var app = express();
+
+mongoose.connect('mongodb+srv://superdev:Xp10UqRxqWHqz9Lb@2013110644-yuu.itnxefc.mongodb.net/refulapi?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(logger('dev'));
 app.use(express.json());
